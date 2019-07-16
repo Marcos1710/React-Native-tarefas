4d4982cc6bdf152b44ae3811af1f81cab9d5b6a0
Marcos Santos <marcossamuel1710@outlook.com>
2019-07-16 11:28:29 -0300

    criado componente agenda, ainda fixo

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { registerRootComponent } from 'expo';
 import { activateKeepAwake } from 'expo-keep-awake';
 
-import App from '../../App';
+import Agenda from './src/screens/Agenda'
 
 if (__DEV__) {
   activateKeepAwake();
 }
 
-registerRootComponent(App);
+registerRootComponent(Agenda);

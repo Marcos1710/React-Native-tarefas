5de04fadb0b06ebadade7a625a001747b74cbf3a
Marcos Santos <marcossamuel1710@outlook.com>
2019-07-18 13:43:46 -0300

    criado componente modal para add tasks

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -14,6 +14,8 @@ import todayImage from '../../assets/imgs/today.jpg'
 import commonStyles from '../CommonStyle'
 import Task from '../components/Task'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import ActionButton from 'react-native-action-button'
+import AddTask from './addTasks'
 
 export default class Agenda extends Component {
 
@@ -41,7 +43,20 @@ export default class Agenda extends Component {
             estimateAt: new Date(), doneAt: null },
         ],
         visibleTasks: [ ],
-        showDoneTasks: true
+        showDoneTasks: true,
+        showAddTask: false
+    }
+
+    addTask = task => {
+        const tasks = [...this.state.tasks]
+        tasks.push({
+            id: Math.random(),
+            desc: task.desc,
+            estimateAt: task.date,
+            doneAt: null
+        })
+
+        this.setState({ task, showAddTask: false }, this.filterTaks )
     }
 
     filterTaks = () => {
@@ -78,6 +93,9 @@ export default class Agenda extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <AddTask isVisible={this.state.showAddTask}
+                    onSave={this.addTask}
+                    onCancel={() => {this.setState({ showAddTask: false }) }} />
                 <ImageBackground source={todayImage} style={styles.background} >
                     <View style={styles.iconBar}>
                         <TouchableOpacity onPress={this.toggleFilter}>
@@ -97,6 +115,8 @@ export default class Agenda extends Component {
                         renderItem={({ item }) => 
                             <Task {...item}  toggleTask={this.toggleTask} />} />
                 </View>
+                <ActionButton buttonColor={commonStyles.colors.today} 
+                    onPress={() => {this.setState({ showAddTask: true }) }} />
             </View>
         )
     }

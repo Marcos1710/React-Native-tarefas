875a6ff3c1e058bcb3bbc54729fe344c4a680424
Marcos Santos <marcossamuel1710@outlook.com>
2019-07-30 13:49:15 -0300

    criado component menu ja fazendo todos os filtros de tarefas por periodo de tempo

--- src/Navigator.js
+++ src/Navigator.js
@@ -2,12 +2,62 @@ import React from 'react'
 import {
     createStackNavigator,
     createAppContainer,
+    createDrawerNavigator
 } from 'react-navigation'
 import { StyleSheet } from 'react-native'
 import Agenda from './screens/Agenda'
 import Auth from './screens/Auth'
+import CommonStyles from './CommonStyle'
 
 // https://docs.expo.io/versions/latest/react-native/navigation/
+
+const menuRoutes = {
+    Today: {
+        name: 'Today',
+        screen: props => <Agenda tittle='Hoje' daysAhead={0} {...props} />,
+        navigationOptions: {
+            tittle: 'Hoje'
+        }
+    },
+    Tomorrow: {
+        name: 'Tomorrow',
+        screen: props => <Agenda tittle='Amanhã' daysAhead={1} {...props} />,
+        navigationOptions: {
+            tittle: 'Amanhã'
+        }
+    },
+    Week: {
+        name: 'Week',
+        screen: props => <Agenda tittle='Semana' daysAhead={7} {...props} />,
+        navigationOptions: {
+            tittle: 'Semana'
+        }
+    },
+    Month: {
+        name: 'Month',
+        screen: props => 
+            <Agenda title='Mês' daysAhead={30} {...props} />,
+        navigationOptions: {
+            title: 'Mês'
+        }
+    }
+}
+
+const menuConfig = {
+    initialRouteName: 'Today',
+    contentOptions: {
+        labelStyle: {
+            fontFamily: CommonStyles.fontFamily,
+            fontWeight: 'normal',
+            fontSize: 20
+        },
+        activeLabelStyle: {
+            color: '#080'
+        }
+    }
+}
+
+const MenuNavigator = createDrawerNavigator(menuRoutes, menuConfig)
 
 const Navigator = createStackNavigator({
     Auth: {
@@ -17,7 +67,7 @@ const Navigator = createStackNavigator({
     },
     Home: {
         name: 'Home',
-        screen: Agenda,
+        screen: MenuNavigator,
         navigationOptions: { header: null }
     },
 });
--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -10,7 +10,6 @@ import {
 } from 'react-native'
 import moment, { months } from 'moment'
 import 'moment/locale/pt-br'
-import todayImage from '../../assets/imgs/today.jpg'
 import commonStyles from '../CommonStyle'
 import Task from '../components/Task'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -21,6 +20,11 @@ import {
     showError
 } from '../Common'
 import Axios from 'axios';
+
+import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
 
 export default class Agenda extends Component {
 
@@ -85,6 +89,7 @@ export default class Agenda extends Component {
     loadTasks = async () => {
         try {
             const maxDate = moment()
+                .add({ days: this.props.daysAhead })
                 .format('YYYY-MM-DD 23:59')
             const res = await Axios.get(`${server}/tasks?date=${maxDate}`)
             this.setState({ tasks: res.data }, this.filterTasks)
@@ -94,20 +99,45 @@ export default class Agenda extends Component {
     }
 
     render() {
+        let styleColor = null
+        let image = null
+
+        switch(this.props.daysAhead) {
+            case 0:
+                styleColor = commonStyles.colors.today
+                image = todayImage
+                break
+            case 1:
+                styleColor = commonStyles.colors.tomorrow
+                image = tomorrowImage
+                break
+            case 7:
+                styleColor = commonStyles.colors.week
+                image = weekImage
+                break
+            default:
+                styleColor = commonStyles.colors.month
+                image = monthImage
+                break
+        }
+
         return (
             <View style={styles.container}>
                 <AddTask isVisible={this.state.showAddTask}
                     onSave={this.addTask}
                     onCancel={() => {this.setState({ showAddTask: false }) }} />
-                <ImageBackground source={todayImage} style={styles.background} >
+                <ImageBackground source={image} style={styles.background} >
                     <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
+                            <Icon name='bars' size={20} color={commonStyles.colors.secondary} />
+                        </TouchableOpacity>
                         <TouchableOpacity onPress={this.toggleFilter}>
                             <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
                                 size={20} color={commonStyles.colors.secondary} />
                         </TouchableOpacity>
                     </View>
                     <View style={styles.titleBar} >
-                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.title}>{this.props.title}</Text>
                         <Text style={styles.subtitle}>
                             {moment().locale('pt-br').format('ddd, D [de] MMMM')}
                         </Text>
@@ -118,7 +148,7 @@ export default class Agenda extends Component {
                         renderItem={({ item }) => 
                             <Task {...item}  onToggleTask={this.toggleTask} onDelete={this.deleteTask} />} />
                 </View>
-                <ActionButton buttonColor={commonStyles.colors.today} 
+                <ActionButton buttonColor={styleColor} 
                     onPress={() => {this.setState({ showAddTask: true }) }} />
             </View>
         )
@@ -157,6 +187,6 @@ const styles = StyleSheet.create({
         marginTop: Platform.OS === 'ios' ? 30 : 30,
         marginHorizontal: 20,
         flexDirection: 'row',
-        justifyContent: 'flex-end'
+        justifyContent: 'space-between'
     }
 })
